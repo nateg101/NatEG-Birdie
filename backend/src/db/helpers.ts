c2bff8bc8b0270db2cb547e2cfe49d7bef787612
Nat Elliott-Green <natelliottgreen34@gmail.com>
2023-02-25 17:18:14 +0000

    formatted backend

--- backend/src/db/helpers.ts
+++ backend/src/db/helpers.ts
@@ -2,14 +2,14 @@ export function eventQueryBuilder(start: string, end: string): string {
   let dbQuery = 'SELECT payload FROM events';
 
   if (start) {
-    dbQuery = `${dbQuery} WHERE timestamp > STR_TO_DATE('${start}','%Y-%m-%d %H:%i:%s')`;
+    dbQuery = `${dbQuery} WHERE timestamp >= STR_TO_DATE('${start}','%Y-%m-%d %H:%i:%s')`;
   }
 
   if (end) {
     if (start) {
-      dbQuery = `${dbQuery} AND timestamp < STR_TO_DATE('${end}','%Y-%m-%d %H:%i:%s')`;
+      dbQuery = `${dbQuery} AND timestamp <= STR_TO_DATE('${end}','%Y-%m-%d %H:%i:%s')`;
     } else {
-      dbQuery = `${dbQuery} WHERE timestamp < STR_TO_DATE('${end}','%Y-%m-%d %H:%i:%s')`;
+      dbQuery = `${dbQuery} WHERE timestamp <= STR_TO_DATE('${end}','%Y-%m-%d %H:%i:%s')`;
     }
   }
 
