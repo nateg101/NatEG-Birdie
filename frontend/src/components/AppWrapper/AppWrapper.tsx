01e17571aac340966aba2a20b8a4882f0bd711ed
Nat Elliott-Green <natelliottgreen34@gmail.com>
2023-02-25 00:02:11 +0000

    ran prettier over the components to get a lovely format

--- frontend/src/components/AppWrapper/AppWrapper.tsx
+++ frontend/src/components/AppWrapper/AppWrapper.tsx
@@ -1,22 +1,22 @@
 import styled from 'styled-components';
 
 const AppWrapper = styled.div`
-    align-items: center;
-    justify-content: center;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  h1 {
+    font-size: 3rem;
+    line-height: 1.1;
+    font-weight: 600;
+    margin-top: 5rem;
+    margin-bottom: 3rem;
+  }
+
+  p,
+  h1 {
     text-align: center;
+  }
+`;
 
-    
-    h1 {
-        font-size: 3rem;
-        line-height: 1.1;
-        font-weight: 600;
-        margin-top: 5rem;
-        margin-bottom: 3rem;
-    }
-    
-    p, h1 {
-        text-align: center;
-    }
-    `;
-
-export default AppWrapper
+export default AppWrapper;
--- frontend/src/containers/HomePage.tsx
+++ frontend/src/containers/HomePage.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import AppWrapper from '../components/AppWrapper/AppWrapper';
 import LinkButton from '../components/LinkButton/LinkButton';
-import VisitItem from '../components/VisitItem/VisitItem';
 
 function HomePage(): React.ReactElement {
   return (
-    <AppWrapper className='home-page'>
-        <h1>Welcome to Birdie</h1>
-        <p>Please click below to view care visits</p>
-        <LinkButton className='view-care-redirect' to='visits'> View Care Visits</LinkButton>
-        <VisitItem />
+    <AppWrapper className="home-page">
+      <h1>Welcome to Birdie</h1>
+      <p>Please click below to view care visits</p>
+      <LinkButton className="view-care-redirect" to="visits">
+        View Care Visits
+      </LinkButton>
     </AppWrapper>
   );
 }
