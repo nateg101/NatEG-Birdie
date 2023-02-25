01900c28df9748ff2000b5fca64841c509d9fa39
Nat Elliott-Green <natelliottgreen34@gmail.com>
2023-02-25 00:00:28 +0000

    added data fetching for VisitsList so it's now functional - NEEDS Tests

--- frontend/src/containers/VisitsList.tsx
+++ frontend/src/containers/VisitsList.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { VisitItem } from '../components/VisitItem/VisitItem';
 
 function VisitsListWrapper(): React.ReactElement {
-    return (
-        <div className='visits-wrapper'>Visits</div>
-    );
+  const [loading, setLoading] = useState(true);
+  const [visits, setVisits] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/events')
+      .then((res) => res.json())
+      .then((results) => {
+        setVisits(results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(true);
+      });
+  }, []);
+
+  return (
+    <div className="visits-wrapper">
+      {loading ? (
+        <div className="loading">Loading ...</div>
+      ) : (
+        visits.length > 0 &&
+        visits.map((visitData: any) => {
+          return <VisitItem visit={visitData} />;
+        })
+      )}
+    </div>
+  );
 }
 
 export default VisitsListWrapper;
